4576f637ea5a505254c1391f9ffe8f1287bfdf1b
Kalpesh Padmawar <kalpeshpadmawar22@gmail.com>
2026-06-19 20:22:13 +0530

    feat: restore detailed 3D face mesh and orient right-side up

--- app.js
+++ app.js
@@ -179,131 +179,113 @@ function initMagneticGlow() {
    3D Wireframe Face Renderers (Logo & Hero profile avatar - Realistic low-poly)
    ========================================================================= */
 function init3DFaceRenderers() {
-  // 3D low-poly wireframe of a working man coding at a desk (52 vertices)
+  // Realistic 3D low-poly face mesh nodes (59 vertices)
   const FACE_VERTICES = [
-    // Head (0-7)
-    {x: -0.12, y: 0.85, z: 0.15},    // 0
-    {x: 0.12, y: 0.85, z: 0.15},     // 1
-    {x: -0.12, y: 0.85, z: -0.1},    // 2
-    {x: 0.12, y: 0.85, z: -0.1},     // 3
-    {x: -0.1, y: 0.65, z: 0.15},     // 4
-    {x: 0.1, y: 0.65, z: 0.15},      // 5
-    {x: -0.1, y: 0.65, z: -0.1},     // 6
-    {x: 0.1, y: 0.65, z: -0.1},      // 7
-
-    // Neck (8-9)
-    {x: 0, y: 0.65, z: 0.02},        // 8
-    {x: 0, y: 0.55, z: 0.02},        // 9
-
-    // Torso/Shoulders/Spine (10-15)
-    {x: -0.35, y: 0.5, z: 0.0},      // 10: Left Shoulder
-    {x: 0.35, y: 0.5, z: 0.0},       // 11: Right Shoulder
-    {x: -0.18, y: -0.15, z: -0.1},   // 12: Left Hip
-    {x: 0.18, y: -0.15, z: -0.1},    // 13: Right Hip
-    {x: 0, y: 0.32, z: 0.12},        // 14: Chest Center
-    {x: 0, y: 0.22, z: -0.06},       // 15: Spine Center
-
-    // Left Arm (16-17)
-    {x: -0.4, y: 0.26, z: 0.22},     // 16: Left Elbow
-    {x: -0.18, y: 0.14, z: 0.45},    // 17: Left Wrist
-
-    // Right Arm (18-19)
-    {x: 0.4, y: 0.26, z: 0.22},      // 18: Right Elbow
-    {x: 0.18, y: 0.14, z: 0.45},     // 19: Right Wrist
-
-    // Legs (20-25)
-    {x: -0.22, y: -0.22, z: 0.25},   // 20: Left Knee
-    {x: 0.22, y: -0.22, z: 0.25},    // 21: Right Knee
-    {x: -0.22, y: -0.85, z: 0.2},    // 22: Left Ankle/Foot
-    {x: 0.22, y: -0.85, z: 0.2},     // 23: Right Ankle/Foot
-    {x: -0.2, y: -0.18, z: -0.2},    // 24: Left seat area
-    {x: 0.2, y: -0.18, z: -0.2},     // 25: Right seat area
-
-    // Desk (26-29)
-    {x: -0.85, y: 0.06, z: 0.6},     // 26: Front-Left Corner
-    {x: 0.85, y: 0.06, z: 0.6},      // 27: Front-Right Corner
-    {x: -0.85, y: 0.06, z: 0.25},    // 28: Back-Left Corner
-    {x: 0.85, y: 0.06, z: 0.25},     // 29: Back-Right Corner
-
-    // Laptop (30-37)
-    {x: -0.2, y: 0.08, z: 0.52},     // 30: Base Front-Left
-    {x: 0.2, y: 0.08, z: 0.52},      // 31: Base Front-Right
-    {x: -0.18, y: 0.08, z: 0.38},    // 32: Hinge Left
-    {x: 0.18, y: 0.08, z: 0.38},     // 33: Hinge Right
-    {x: -0.18, y: 0.34, z: 0.3},     // 34: Screen Top-Left
-    {x: 0.18, y: 0.34, z: 0.3},      // 35: Screen Top-Right
-    {x: 0, y: 0.2, z: 0.34},         // 36: Screen Center
-    {x: 0, y: 0.08, z: 0.45},        // 37: Keyboard Center
-
-    // Chair (38-45)
-    {x: -0.25, y: 0.65, z: -0.25},   // 38: Backrest Top-Left
-    {x: 0.25, y: 0.65, z: -0.25},    // 39: Backrest Top-Right
-    {x: -0.22, y: 0.08, z: -0.22},   // 40: Backrest Bottom-Left
-    {x: 0.22, y: 0.08, z: -0.22},    // 41: Backrest Bottom-Right
-    {x: 0, y: -0.85, z: -0.22},      // 42: Wheels Center
-    {x: 0, y: -0.38, z: -0.22},      // 43: Stem Bottom
-    {x: -0.18, y: -0.85, z: -0.35},  // 44: Wheel Back-Left
-    {x: 0.18, y: -0.85, z: -0.35},   // 45: Wheel Back-Right
-
-    // Desk Legs (46-49)
-    {x: -0.85, y: -0.85, z: 0.6},    // 46: Leg FL Bottom
-    {x: 0.85, y: -0.85, z: 0.6},     // 47: Leg FR Bottom
-    {x: -0.85, y: -0.85, z: 0.25},   // 48: Leg BL Bottom
-    {x: 0.85, y: -0.85, z: 0.25},    // 49: Leg BR Bottom
-
-    // Additional wheels (50-51)
-    {x: -0.18, y: -0.85, z: -0.1},   // 50: Wheel Front-Left
-    {x: 0.18, y: -0.85, z: -0.1}     // 51: Wheel Front-Right
+    {x: 0, y: 1.5, z: 0.2},         // 0: Center top forehead
+    {x: -0.4, y: 1.4, z: 0.15},      // 1: Left top forehead
+    {x: 0.4, y: 1.4, z: 0.15},       // 2: Right top forehead
+    {x: -0.7, y: 1.1, z: 0.05},      // 3: Left temple high
+    {x: 0.7, y: 1.1, z: 0.05},       // 4: Right temple high
+    {x: 0, y: 1.0, z: 0.4},          // 5: Mid forehead center
+    {x: -0.35, y: 0.95, z: 0.35},    // 6: Mid forehead left
+    {x: 0.35, y: 0.95, z: 0.35},     // 7: Mid forehead right
+    {x: -0.75, y: 0.8, z: 0.15},     // 8: Temple left mid
+    {x: 0.75, y: 0.8, z: 0.15},      // 9: Temple right mid
+    {x: -0.6, y: 0.6, z: 0.38},      // 10: Brow left outer
+    {x: -0.32, y: 0.65, z: 0.46},    // 11: Brow left middle
+    {x: -0.1, y: 0.58, z: 0.48},     // 12: Brow left inner
+    {x: 0.1, y: 0.58, z: 0.48},      // 13: Brow right inner
+    {x: 0.32, y: 0.65, z: 0.46},     // 14: Brow right middle
+    {x: 0.6, y: 0.6, z: 0.38},       // 15: Brow right outer
+    {x: 0, y: 0.48, z: 0.54},        // 16: Nose bridge top
+    {x: 0, y: 0.15, z: 0.72},        // 17: Nose bridge mid
+    {x: 0, y: -0.22, z: 0.95},       // 18: Nose tip
+    {x: 0, y: -0.38, z: 0.76},       // 19: Nose base center
+    {x: -0.22, y: -0.32, z: 0.72},   // 20: Nose left nostril outer
+    {x: 0.22, y: -0.32, z: 0.72},    // 21: Nose right nostril outer
+    {x: -0.1, y: -0.38, z: 0.74},     // 22: Nose left nostril bottom
+    {x: 0.1, y: -0.38, z: 0.74},      // 23: Nose right nostril bottom
+    {x: -0.16, y: 0.32, z: 0.48},    // 24: Left eye inner corner
+    {x: -0.48, y: 0.32, z: 0.38},    // 25: Left eye outer corner
+    {x: -0.32, y: 0.4, z: 0.46},     // 26: Left eye top
+    {x: -0.32, y: 0.24, z: 0.44},    // 27: Left eye bottom
+    {x: -0.32, y: 0.32, z: 0.43},    // 28: Left pupil/center
+    {x: 0.16, y: 0.32, z: 0.48},     // 29: Right eye inner corner
+    {x: 0.48, y: 0.32, z: 0.38},     // 30: Right eye outer corner
+    {x: 0.32, y: 0.4, z: 0.46},      // 31: Right eye top
+    {x: 0.32, y: 0.24, z: 0.44},     // 32: Right eye bottom
+    {x: 0.32, y: 0.32, z: 0.43},     // 33: Right pupil/center
+    {x: -0.82, y: 0.2, z: 0.22},     // 34: Cheekbone left outer
+    {x: 0.82, y: 0.2, z: 0.22},      // 35: Cheekbone right outer
+    {x: -0.52, y: -0.05, z: 0.42},   // 36: Cheek left mid
+    {x: 0.52, y: -0.05, z: 0.42},    // 37: Cheek right mid
+    {x: -0.32, y: -0.38, z: 0.58},   // 38: Nasolabial fold left
+    {x: 0.32, y: -0.38, z: 0.58},    // 39: Nasolabial fold right
+    {x: -0.08, y: -0.5, z: 0.69},    // 40: Upper lip top center L
+    {x: 0.08, y: -0.5, z: 0.69},     // 41: Upper lip top center R
+    {x: -0.28, y: -0.58, z: 0.6},    // 42: Mouth corner left
+    {x: 0.28, y: -0.58, z: 0.6},     // 43: Mouth corner right
+    {x: 0, y: -0.56, z: 0.66},       // 44: Lip join center
+    {x: 0, y: -0.7, z: 0.64},        // 45: Lower lip bottom center
+    {x: -0.16, y: -0.53, z: 0.64},   // 46: Upper lip left mid
+    {x: 0.16, y: -0.53, z: 0.64},    // 47: Upper lip right mid
+    {x: -0.14, y: -0.65, z: 0.62},   // 48: Lower lip left mid
+    {x: 0.14, y: -0.65, z: 0.62},    // 49: Lower lip right mid
+    {x: 0, y: -1.0, z: 0.52},        // 50: Chin tip top
+    {x: 0, y: -1.2, z: 0.45},        // 51: Chin tip bottom
+    {x: 0, y: -1.35, z: 0.25},       // 52: Chin base
+    {x: -0.68, y: -0.78, z: 0.05},   // 53: Jaw angle left
+    {x: 0.68, y: -0.78, z: 0.05},    // 54: Jaw angle right
+    {x: -0.4, y: -1.02, z: 0.28},    // 55: Jaw mid left
+    {x: 0.4, y: -1.02, z: 0.28},     // 56: Jaw mid right
+    {x: -0.18, y: -0.9, z: 0.46},    // 57: Chin left crease
+    {x: 0.18, y: -0.9, z: 0.46}      // 58: Chin right crease
   ];
 
-  // Wireframe facet edge connections
+  // Wireframe facet edge connections (110 connections)
   const FACE_CONNECTIONS = [
-    // Head (cube wireframe)
-    [0, 1], [1, 5], [5, 4], [4, 0],   // Front Face
-    [2, 3], [3, 7], [7, 6], [6, 2],   // Back Face
-    [0, 2], [1, 3], [4, 6], [5, 7],   // Connectors
-
-    // Neck
-    [4, 8], [5, 8], [6, 8], [7, 8],
-    [8, 9],
-
-    // Shoulders & Torso
-    [10, 11], [9, 10], [9, 11],
-    [9, 15], [15, 12], [15, 13],
-    [10, 14], [11, 14], [14, 12], [14, 13], [12, 13],
-
-    // Left Arm
-    [10, 16], [16, 17], [17, 30], [17, 32],
-
-    // Right Arm
-    [11, 18], [18, 19], [19, 31], [19, 33],
-
-    // Legs
-    [12, 20], [13, 21], [20, 22], [21, 23],
-    [12, 24], [13, 25], [24, 25],
-
-    // Desk Outline
-    [26, 27], [27, 29], [29, 28], [28, 26],
-
-    // Desk Legs
-    [26, 46], [27, 47], [28, 48], [29, 49],
-
-    // Laptop Base
-    [30, 31], [31, 33], [33, 32], [32, 30],
-    // Laptop Screen
-    [32, 34], [33, 35], [34, 35],
-    // Keyboard / Screen details (diagonals/coding look)
-    [30, 37], [31, 37], [32, 37], [33, 37],
-    [34, 36], [35, 36], [32, 36], [33, 36],
-
-    // Chair Backrest
-    [38, 39], [39, 41], [41, 40], [40, 38],
-    // Chair Backrest to Seat Connectors
-    [40, 24], [41, 25],
-    // Chair Stem
-    [24, 43], [25, 43], [43, 42],
-    // Chair Wheels star pattern
-    [42, 44], [42, 45], [42, 50], [42, 51]
+    // Forehead and hairline
+    [0, 1], [1, 3], [0, 2], [2, 4],
+    // Forehead internal
+    [0, 5], [1, 6], [2, 7], [3, 8], [4, 9],
+    [5, 6], [6, 8], [5, 7], [7, 9],
+    [1, 5], [2, 5],
+    // Forehead to brows
+    [5, 12], [5, 13], [6, 11], [7, 14], [8, 10], [9, 15],
+    // Brow line
+    [10, 11], [11, 12], [12, 13], [13, 14], [14, 15],
+    // Nose bridge
+    [12, 16], [13, 16], [16, 17], [17, 18], [18, 19],
+    // Nostrils
+    [17, 20], [17, 21], [18, 20], [18, 21], [20, 22], [21, 23], [22, 19], [23, 19],
+    // Left Eye
+    [24, 26], [26, 25], [25, 27], [27, 24],
+    [28, 24], [28, 25], [28, 26], [28, 27],
+    // Left Eye connections to Brow & Cheek
+    [11, 26], [12, 24], [10, 25], [24, 16], [24, 17],
+    // Right Eye
+    [29, 31], [31, 30], [30, 32], [32, 29],
+    [33, 29], [33, 30], [33, 31], [33, 32],
+    // Right Eye connections to Brow & Cheek
+    [14, 31], [13, 29], [15, 30], [29, 16], [29, 17],
+    // Cheekbones & outer boundaries
+    [3, 34], [4, 35], [25, 34], [30, 35],
+    [34, 36], [35, 37], [25, 36], [30, 37],
+    [36, 38], [37, 39], [36, 42], [37, 43],
+    [20, 38], [21, 39], [19, 38], [19, 39],
+    // Mouth & Lips
+    [40, 41], [40, 46], [41, 47], [46, 42], [47, 43],
+    [42, 44], [43, 44], [40, 44], [41, 44],
+    [42, 48], [43, 49], [48, 45], [49, 45], [44, 45],
+    [19, 40], [19, 41], [20, 46], [21, 47],
+    // Jaw & Chin boundary
+    [53, 55], [55, 51], [54, 56], [56, 51], [51, 52],
+    [34, 53], [35, 54],
+    // Lower cheeks to jaw
+    [36, 53], [37, 54], [36, 55], [37, 56],
+    // Lips to chin
+    [42, 57], [43, 58], [48, 57], [49, 58],
+    [45, 50], [57, 50], [58, 50], [50, 51], [57, 55], [58, 56]
   ];
 
   // Helper function to render wireframe face on a canvas
@@ -356,7 +338,7 @@ function init3DFaceRenderers() {
       const currentAngleY = angleY + (mousePercentX * 0.95);
       const currentAngleX = mousePercentY * -0.95;
 
-      // Map vertices with 3D projection formulas
+      // Map vertices with 3D projection formulas (inverted Y to render right-side up)
       const projected = FACE_VERTICES.map(v => {
         // Rotate Y
         let x1 = v.x * Math.cos(currentAngleY) - v.z * Math.sin(currentAngleY);
@@ -372,7 +354,7 @@ function init3DFaceRenderers() {
         const projScale = scale * fov / (z2 + distance);
         
         const px = x1 * projScale + width / 2;
-        const py = y2 * projScale + height / 2;
+        const py = -y2 * projScale + height / 2;
         
         return { x: px, y: py };
       });
