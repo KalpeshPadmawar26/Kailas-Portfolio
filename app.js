1eb95407099cfbaf25e29084a86d94c0aa611e62
Kalpesh Padmawar <kalpeshpadmawar22@gmail.com>
2026-06-19 20:19:55 +0530

    feat: upgrade 3D face wireframe mesh to high-fidelity anatomical model

--- app.js
+++ app.js
@@ -179,73 +179,113 @@ function initMagneticGlow() {
    3D Wireframe Face Renderers (Logo & Hero profile avatar - Realistic low-poly)
    ========================================================================= */
 function init3DFaceRenderers() {
-  // Realistic 3D low-poly face mesh nodes (39 vertices)
+  // Realistic 3D low-poly face mesh nodes (59 vertices)
   const FACE_VERTICES = [
-    {x: 0, y: 1.25, z: 0.45},       // 0: Forehead Center
-    {x: -0.4, y: 1.15, z: 0.35},     // 1: Forehead Left
-    {x: 0.4, y: 1.15, z: 0.35},      // 2: Forehead Right
-    {x: -0.75, y: 0.85, z: 0.2},     // 3: Temple Left
-    {x: 0.75, y: 0.85, z: 0.2},      // 4: Temple Right
-    {x: -0.6, y: 0.55, z: 0.35},     // 5: Brow Left Outer
-    {x: -0.35, y: 0.6, z: 0.45},     // 6: Brow Left Middle
-    {x: -0.1, y: 0.55, z: 0.48},     // 7: Brow Left Inner
-    {x: 0.1, y: 0.55, z: 0.48},      // 8: Brow Right Inner
-    {x: 0.35, y: 0.6, z: 0.45},      // 9: Brow Right Middle
-    {x: 0.6, y: 0.55, z: 0.35},      // 10: Brow Right Outer
-    {x: 0, y: 0.5, z: 0.55},         // 11: Nose Bridge
-    {x: 0, y: 0.1, z: 0.72},         // 12: Nose Mid-bridge
-    {x: 0, y: -0.25, z: 0.95},       // 13: Nose Tip
-    {x: 0, y: -0.4, z: 0.75},        // 14: Nose Base
-    {x: -0.25, y: -0.35, z: 0.72},   // 15: Nostril Left
-    {x: 0.25, y: -0.35, z: 0.72},    // 16: Nostril Right
-    {x: -0.5, y: 0.3, z: 0.4},       // 17: Eye Left Outer corner
-    {x: -0.15, y: 0.3, z: 0.5},      // 18: Eye Left Inner corner
-    {x: 0.15, y: 0.3, z: 0.5},       // 19: Eye Right Inner corner
-    {x: 0.5, y: 0.3, z: 0.4},        // 20: Eye Right Outer corner
-    {x: -0.32, y: 0.38, z: 0.48},    // 21: Eye Left Upper lid
-    {x: -0.32, y: 0.22, z: 0.45},    // 22: Eye Left Lower lid
-    {x: 0.32, y: 0.38, z: 0.48},     // 23: Eye Right Upper lid
-    {x: 0.32, y: 0.22, z: 0.45},     // 24: Eye Right Lower lid
-    {x: -0.55, y: -0.1, z: 0.4},     // 25: Cheek Left Mid
-    {x: 0.55, y: -0.1, z: 0.4},      // 26: Cheek Right Mid
-    {x: -0.85, y: -0.05, z: 0.25},   // 27: Cheek Left Outer
-    {x: 0.85, y: -0.05, z: 0.25},    // 28: Cheek Right Outer
-    {x: 0, y: -0.52, z: 0.68},       // 29: Lip Upper Center
-    {x: 0, y: -0.72, z: 0.65},       // 30: Lip Lower Center
-    {x: -0.3, y: -0.62, z: 0.58},    // 31: Mouth Left Corner
-    {x: 0.3, y: -0.62, z: 0.58},     // 32: Mouth Right Corner
-    {x: 0, y: -1.2, z: 0.45},        // 33: Chin Tip
-    {x: 0, y: -1.3, z: 0.3},         // 34: Chin Base
-    {x: -0.6, y: -0.85, z: 0.1},     // 35: Jaw Angle Left
-    {x: 0.6, y: -0.85, z: 0.1},      // 36: Jaw Angle Right
-    {x: -0.35, y: -1.05, z: 0.28},   // 37: Jaw Mid Left
-    {x: 0.35, y: -1.05, z: 0.28}     // 38: Jaw Mid Right
+    {x: 0, y: 1.5, z: 0.2},         // 0: Center top forehead
+    {x: -0.4, y: 1.4, z: 0.15},      // 1: Left top forehead
+    {x: 0.4, y: 1.4, z: 0.15},       // 2: Right top forehead
+    {x: -0.7, y: 1.1, z: 0.05},      // 3: Left temple high
+    {x: 0.7, y: 1.1, z: 0.05},       // 4: Right temple high
+    {x: 0, y: 1.0, z: 0.4},          // 5: Mid forehead center
+    {x: -0.35, y: 0.95, z: 0.35},    // 6: Mid forehead left
+    {x: 0.35, y: 0.95, z: 0.35},     // 7: Mid forehead right
+    {x: -0.75, y: 0.8, z: 0.15},     // 8: Temple left mid
+    {x: 0.75, y: 0.8, z: 0.15},      // 9: Temple right mid
+    {x: -0.6, y: 0.6, z: 0.38},      // 10: Brow left outer
+    {x: -0.32, y: 0.65, z: 0.46},    // 11: Brow left middle
+    {x: -0.1, y: 0.58, z: 0.48},     // 12: Brow left inner
+    {x: 0.1, y: 0.58, z: 0.48},      // 13: Brow right inner
+    {x: 0.32, y: 0.65, z: 0.46},     // 14: Brow right middle
+    {x: 0.6, y: 0.6, z: 0.38},       // 15: Brow right outer
+    {x: 0, y: 0.48, z: 0.54},        // 16: Nose bridge top
+    {x: 0, y: 0.15, z: 0.72},        // 17: Nose bridge mid
+    {x: 0, y: -0.22, z: 0.95},       // 18: Nose tip
+    {x: 0, y: -0.38, z: 0.76},       // 19: Nose base center
+    {x: -0.22, y: -0.32, z: 0.72},   // 20: Nose left nostril outer
+    {x: 0.22, y: -0.32, z: 0.72},    // 21: Nose right nostril outer
+    {x: -0.1, y: -0.38, z: 0.74},     // 22: Nose left nostril bottom
+    {x: 0.1, y: -0.38, z: 0.74},      // 23: Nose right nostril bottom
+    {x: -0.16, y: 0.32, z: 0.48},    // 24: Left eye inner corner
+    {x: -0.48, y: 0.32, z: 0.38},    // 25: Left eye outer corner
+    {x: -0.32, y: 0.4, z: 0.46},     // 26: Left eye top
+    {x: -0.32, y: 0.24, z: 0.44},    // 27: Left eye bottom
+    {x: -0.32, y: 0.32, z: 0.43},    // 28: Left pupil/center
+    {x: 0.16, y: 0.32, z: 0.48},     // 29: Right eye inner corner
+    {x: 0.48, y: 0.32, z: 0.38},     // 30: Right eye outer corner
+    {x: 0.32, y: 0.4, z: 0.46},      // 31: Right eye top
+    {x: 0.32, y: 0.24, z: 0.44},     // 32: Right eye bottom
+    {x: 0.32, y: 0.32, z: 0.43},     // 33: Right pupil/center
+    {x: -0.82, y: 0.2, z: 0.22},     // 34: Cheekbone left outer
+    {x: 0.82, y: 0.2, z: 0.22},      // 35: Cheekbone right outer
+    {x: -0.52, y: -0.05, z: 0.42},   // 36: Cheek left mid
+    {x: 0.52, y: -0.05, z: 0.42},    // 37: Cheek right mid
+    {x: -0.32, y: -0.38, z: 0.58},   // 38: Nasolabial fold left
+    {x: 0.32, y: -0.38, z: 0.58},    // 39: Nasolabial fold right
+    {x: -0.08, y: -0.5, z: 0.69},    // 40: Upper lip top center L
+    {x: 0.08, y: -0.5, z: 0.69},     // 41: Upper lip top center R
+    {x: -0.28, y: -0.58, z: 0.6},    // 42: Mouth corner left
+    {x: 0.28, y: -0.58, z: 0.6},     // 43: Mouth corner right
+    {x: 0, y: -0.56, z: 0.66},       // 44: Lip join center
+    {x: 0, y: -0.7, z: 0.64},        // 45: Lower lip bottom center
+    {x: -0.16, y: -0.53, z: 0.64},   // 46: Upper lip left mid
+    {x: 0.16, y: -0.53, z: 0.64},    // 47: Upper lip right mid
+    {x: -0.14, y: -0.65, z: 0.62},   // 48: Lower lip left mid
+    {x: 0.14, y: -0.65, z: 0.62},    // 49: Lower lip right mid
+    {x: 0, y: -1.0, z: 0.52},        // 50: Chin tip top
+    {x: 0, y: -1.2, z: 0.45},        // 51: Chin tip bottom
+    {x: 0, y: -1.35, z: 0.25},       // 52: Chin base
+    {x: -0.68, y: -0.78, z: 0.05},   // 53: Jaw angle left
+    {x: 0.68, y: -0.78, z: 0.05},    // 54: Jaw angle right
+    {x: -0.4, y: -1.02, z: 0.28},    // 55: Jaw mid left
+    {x: 0.4, y: -1.02, z: 0.28},     // 56: Jaw mid right
+    {x: -0.18, y: -0.9, z: 0.46},    // 57: Chin left crease
+    {x: 0.18, y: -0.9, z: 0.46}      // 58: Chin right crease
   ];
 
-  // Wireframe facet edge connections (71 connections)
+  // Wireframe facet edge connections (110 connections)
   const FACE_CONNECTIONS = [
-    // Outer boundary outline
-    [33, 37], [37, 35], [35, 27], [27, 3], [3, 1], [1, 0], [0, 2], [2, 4], [4, 28], [28, 36], [36, 38], [38, 33],
-    // Forehead vertical struts
-    [0, 7], [1, 6], [3, 5], [0, 8], [2, 9], [4, 10],
-    // Brow arcs
-    [7, 6], [6, 5], [8, 9], [9, 10], [7, 8],
-    // Nose vertical bridge & base
-    [11, 12], [12, 13], [13, 14], [14, 29], [12, 15], [12, 16], [15, 14], [16, 14],
-    // Eye Left lines
-    [17, 21], [21, 18], [18, 22], [22, 17], [18, 11], [17, 5], [21, 6], [22, 25],
-    // Eye Right lines
-    [20, 23], [23, 19], [19, 24], [24, 20], [19, 11], [20, 10], [23, 9], [24, 26],
-    // Left cheek networks
-    [5, 27], [27, 25], [15, 25], [25, 31], [25, 35], [31, 37],
-    // Right cheek networks
-    [10, 28], [28, 26], [16, 26], [26, 32], [26, 36], [32, 38],
-    // Nose side transitions
-    [15, 13], [16, 13], [11, 12],
-    // Mouth outline
-    [29, 31], [31, 30], [30, 32], [32, 29], [15, 31], [16, 32], [14, 29],
-    // Chin and lower jaw
-    [30, 33], [37, 33], [38, 33], [37, 34], [38, 34]
+    // Forehead and hairline
+    [0, 1], [1, 3], [0, 2], [2, 4],
+    // Forehead internal
+    [0, 5], [1, 6], [2, 7], [3, 8], [4, 9],
+    [5, 6], [6, 8], [5, 7], [7, 9],
+    [1, 5], [2, 5],
+    // Forehead to brows
+    [5, 12], [5, 13], [6, 11], [7, 14], [8, 10], [9, 15],
+    // Brow line
+    [10, 11], [11, 12], [12, 13], [13, 14], [14, 15],
+    // Nose bridge
+    [12, 16], [13, 16], [16, 17], [17, 18], [18, 19],
+    // Nostrils
+    [17, 20], [17, 21], [18, 20], [18, 21], [20, 22], [21, 23], [22, 19], [23, 19],
+    // Left Eye
+    [24, 26], [26, 25], [25, 27], [27, 24],
+    [28, 24], [28, 25], [28, 26], [28, 27],
+    // Left Eye connections to Brow & Cheek
+    [11, 26], [12, 24], [10, 25], [24, 16], [24, 17],
+    // Right Eye
+    [29, 31], [31, 30], [30, 32], [32, 29],
+    [33, 29], [33, 30], [33, 31], [33, 32],
+    // Right Eye connections to Brow & Cheek
+    [14, 31], [13, 29], [15, 30], [29, 16], [29, 17],
+    // Cheekbones & outer boundaries
+    [3, 34], [4, 35], [25, 34], [30, 35],
+    [34, 36], [35, 37], [25, 36], [30, 37],
+    [36, 38], [37, 39], [36, 42], [37, 43],
+    [20, 38], [21, 39], [19, 38], [19, 39],
+    // Mouth & Lips
+    [40, 41], [40, 46], [41, 47], [46, 42], [47, 43],
+    [42, 44], [43, 44], [40, 44], [41, 44],
+    [42, 48], [43, 49], [48, 45], [49, 45], [44, 45],
+    [19, 40], [19, 41], [20, 46], [21, 47],
+    // Jaw & Chin boundary
+    [53, 55], [55, 51], [54, 56], [56, 51], [51, 52],
+    [34, 53], [35, 54],
+    // Lower cheeks to jaw
+    [36, 53], [37, 54], [36, 55], [37, 56],
+    // Lips to chin
+    [42, 57], [43, 58], [48, 57], [49, 58],
+    [45, 50], [57, 50], [58, 50], [50, 51], [57, 55], [58, 56]
   ];
 
   // Helper function to render wireframe face on a canvas
